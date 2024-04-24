b2bdc0bf2e2e82785ddc90332bb050350d91e146
Von <von@Von-kali.von.com>
2024-04-24 19:48:49 +0300

    updated files

---
+++ src/Components/Landing-components/Application/Application.jsx
@@ -0,0 +1,11 @@
+import './Application.css'
+
+const Application = () => {
+  return (
+    <div className='container'>
+        <h1>Application Form</h1>
+    </div>
+  )
+}
+
+export default Application
---
+++ src/Components/Landing-components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+// import dark_arrow from '../../../assets/dark-arrow.png'
+import white_arrow from '../../../assets/white-arrow.png'
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
+
+const Hero = () => {
+  return (
+    <div className='hero container' id='hero'>
+        <div className='hero-text'>
+            <h1>Transforming Agriculture with CropMaster.</h1>
+            <p>CropMaster offers essential crop information, marketing opportunities, and expert knowledge, revolutionizing farming practices. Join us for an enhanced farming experience</p>
+            <Link to='services' spy={true} smooth={true} duration={500} className='btn'>Explore More<img src={white_arrow} alt=''/></Link>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
