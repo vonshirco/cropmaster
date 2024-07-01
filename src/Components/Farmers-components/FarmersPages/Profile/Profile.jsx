db2bb8d0489c0bbf81f361a63100bca117c150c7
vonshirco <vonshirco.vs@gmail.com>
2024-07-02 02:46:13 +0300

    Implemented Profile Pages

--- src/Components/Farmers-components/FarmersPages/Profile/Profile.jsx
+++ src/Components/Farmers-components/FarmersPages/Profile/Profile.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
-import './Profile.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Profile.css';
 
 const Profile = () => {
-  return (
-    <div className='profile-container'>
-    <h1>Profile: Jane Doe</h1>
-    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-  </div>
-  )
-}
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-export default Profile
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await axios.get('https://tyktyk.pythonanywhere.com/api/user-details/', {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        });
+        setUser(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchUserDetails();
+  }, []);
+
+  return (
+    <div className="profile-container">
+      {loading && <div>Loading...</div>}
+      {error && <div className="error-message">Error: {error.message}</div>}
+      {!loading && !error && (
+        <div className="profile-card">
+          <h1>Profile: {user.first_name} {user.last_name}</h1>
+          <p><strong>Username:</strong> {user.username}</p>
+          <p><strong>Email:</strong> {user.email}</p>
+          <p><strong>Role:</strong> {user.role}</p>
+          <p><strong>Phone Number:</strong> {user.phone_number}</p>
+          <p><strong>Location:</strong> {user.location}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
